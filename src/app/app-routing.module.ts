28debb6135d526fcd924791ae40752f32250506f
HardyJL <j.hardenberg@hotmail.de>
2022-01-01 22:22:32 +0100

    router working
    a
    "

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CertificatesComponent } from './components/certificates/certificates.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DomainsComponent } from './components/domains/domains.component';
+import { HosterComponent } from './components/hoster/hoster.component';
+import { JobsComponent } from './components/jobs/jobs.component';
+import { TargethostsComponent } from './components/targethosts/targethosts.component';
+import { TargetsComponent } from './components/targets/targets.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-  {path: 'dashboard', component: DashboardComponent}
-
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'certificates', component: CertificatesComponent },
+  { path: 'domains', component: DomainsComponent },
+  { path: 'targethosts', component: TargethostsComponent },
+  { path: 'hoster', component: HosterComponent },
+  { path: 'targets', component: TargetsComponent },
+  { path: 'jobs', component: JobsComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes , {preloadingStrategy: PreloadAllModules})],
+  imports: [
+    RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,31 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardItemComponent } from './components/dashboard/dashboard-item/dashboard-item.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { HeaderComponent } from './components/header/header.component';
-import { CertificatesComponent } from './certificates/certificates.component';
 import { DomainsComponent } from './components/domains/domains.component';
-import { TargethostsComponent } from './components/targethosts/targethosts.component';
+import { HeaderComponent } from './components/header/header.component';
 import { HosterComponent } from './components/hoster/hoster.component';
-import { TargetsComponent } from './components/targets/targets.component';
 import { JobsComponent } from './components/jobs/jobs.component';
+import { TargethostsComponent } from './components/targethosts/targethosts.component';
+import { TargetsComponent } from './components/targets/targets.component';
+import { CertificatesComponent } from './components/certificates/certificates.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CertificatesComponent,
     DashboardComponent,
     DashboardItemComponent,
-    HeaderComponent,
-    CertificatesComponent,
     DomainsComponent,
-    TargethostsComponent,
+    HeaderComponent,
     HosterComponent,
-    TargetsComponent,
     JobsComponent,
+    TargethostsComponent,
+    TargetsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-
   ],
   providers: [],
   bootstrap: [AppComponent]
